5ce6ddcfe8f0225eed2dad304ff7643deed9a306
Devsantosh <tech1@wtgconsultants.in>
2024-03-03 00:23:08 +0530

    added file validation with multiple files

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,12 +1,63 @@
 import multer from "multer";
+import fs from "fs";
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./public/temp");
+  destination: (file, req, cb)=>{
+      const uploadedFileDirectoryName = "./public/uploads";
+      if(!fs.existsSync(uploadedFileDirectoryName)){
+        fs.mkdirSync(uploadedFileDirectoryName);
+      }
+      cb(null, uploadedFileDirectoryName);
   },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  },
-});
+  filename: (file, req, cb)=>{
+      const uploadedFileName = `${Date.now()}-${file.originalname}`;
+      cb(null, uploadedFileName);
+  }
+})
 
-export const upload = multer({ storage });
+const upload = multer({storage});
+
+
+// validating uploaded files
+const validateFileAndUpload = (req, res, next) =>{
+
+  upload.array("images", 5)(req, res, (err)=>{
+
+      if(err){
+          return res.status(400).json({message: "More than 5 files are uploaded"});
+      }
+
+      if(!req.files){
+          return res.status(400).json({message: "Files are required"});
+      }
+
+      const files = req.files;
+      const errors = [];
+
+      files.forEach(file => {
+          const allowedType = ['image/png', 'image/jpg', 'image/jpeg', 'image/webp'];
+          const maxSize = 1*1024*1024; //1MB
+
+          if(!allowedType.includes(file.mimetype)){
+              errors.push(`File type invalid ${file.originalname}`);
+          }
+
+          if(file.size > maxSize){
+              errors.push(`File too large ${file.originalname}`);
+          }
+      });
+
+      if(errors.length > 0){
+          files.forEach((file)=>{
+              fs.unlinkSync(file.path);
+          })
+          return res.status(400).json(errors);
+      }
+      
+      req.fiels = files;
+      next();
+  })
+  
+}
+
+export default validateFileAndUpload;
